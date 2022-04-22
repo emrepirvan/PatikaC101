0ca0f51ebe1ec104c5ed82b93ca6b79a73e1a108
Emre Pirvan <emreprvn@gmail.com>
2022-04-23 02:50:07 +0300

    Dictionary Added

---
+++ PatikaC101/DictionaryLesson.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaC101
+{
+    public class DictionaryLesson
+    {
+        public static void Alistirmalar()
+        {
+            Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+            kullanıcılar.Add(10, "ayse Yılmaz");
+            kullanıcılar.Add(12, "ali çetin");
+            kullanıcılar.Add(18, "veli tezgin");
+            kullanıcılar.Add(20, "Özcan deniz");
+
+            //Dizinin elemanlarına erişim
+            Console.WriteLine("----------elemanlarına erişim--------");
+            Console.WriteLine(kullanıcılar[12]);
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine(item);
+            }
+            //Count
+            Console.WriteLine("----------Count--------");
+
+            Console.WriteLine("Count " + kullanıcılar.Count());
+
+            //Contains
+            Console.WriteLine("----------Contains--------");
+            Console.WriteLine(kullanıcılar.ContainsKey(12));
+            Console.WriteLine(kullanıcılar.ContainsValue("ayse Yılmaz"));
+
+
+            //Remove
+            Console.WriteLine("----------Remove--------");
+            kullanıcılar.Remove(12);
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine("values " + item.Value); //Sadece isimleri yazdırıcak
+            }
+            foreach (var item in kullanıcılar)
+            {
+                Console.WriteLine("Keys " +item.Key); //Sadece keyler yazdırıcak
+            }
+
+        }
+    }
+}
--- PatikaC101/Program.cs
+++ PatikaC101/Program.cs
@@ -8,7 +8,7 @@ namespace PatikaC101
     {
         static void Main(string[] args)
         {
-            PatikaC101.ArrayListLesson.Alistirma();
+            PatikaC101.DictionaryLesson.Alistirmalar();
         }
     }
 }
